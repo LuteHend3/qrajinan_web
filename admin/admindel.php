81dd8562d7599c8d03b760bdcac2c5e82a178950
Andrew Patrick de Fretes <andrewbelanda@gmail.com>
2018-12-10 18:06:42 +0700

    delete

--- admin/admindel.php
+++
@@ -1,7 +0,0 @@
-
-<?php 
-include '../koneksi.php';
-$id = $_GET['id_admin'];
-mysqli_query($koneksi,"DELETE FROM admin WHERE id_admin='$id'")or die(mysqli_error());
-header("location:page-admin.php?pesan=hapus");
-?>
--- user/userdel.php
+++
@@ -1,7 +0,0 @@
-
-<?php 
-include '../koneksi.php';
-$id = $_GET['id_user'];
-mysqli_query($koneksi,"DELETE FROM user WHERE id_user='$id'")or die(mysqli_error());
-header("location:page-user.php?pesan=hapus");
-?>
